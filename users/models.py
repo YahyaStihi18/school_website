842b5ef3d3162aaa93fe78e09480d51ccd953c06
YahyaStihi18 <you@example.com>
2020-09-21 13:42:57 +0100

    profil design

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True, blank=True)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    image = models.ImageField()
+    phone = models.BigIntegerField()
+    address = models.CharField(max_length=200)
+    bio = models.TextField()
+    def __str__(self):
+        return self.first_name+' '+self.last_name
