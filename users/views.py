0be823d9da0c316e4f0fbbd5ef6572c0982a0d2d
YahyaStihi18 <you@example.com>
2020-09-21 01:03:17 +0100

    login register profile

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
 
-# Create your views here.
+from .forms import *
+def profile(request):
+    return render(request,'users/profile.html')
+
+    
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterFrom(request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            form.save()
+            messages.success(request,f'{username} registred seccessfuly, Log in with your username/password now.')
+            return redirect ('login')
+    else:
+        form = UserRegisterFrom()
+    return render(request, 'users/register.html', {'form': form})
